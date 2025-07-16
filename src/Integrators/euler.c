2a5e17e3a88e9f4e0094093683b9e1cd4a118e5a
John2003code <abhijnansarkar008@gmail.com>
2025-07-17 03:51:01 +0530

    euler update

---
+++ src/Integrators/euler.c
@@ -0,0 +1,9 @@
+#include "euler.h"
+
+void integrateEulerWithBody(rgdbdy2d* rb, float dt){
+    if(!rb->is_r2_stationary){
+        rb->r2_acc = v_scalar_mul(rb->r2_force, rb->r2_mass);
+        rb->r2_vel = v_scalar_mul(rb->r2_acc , dt);
+        rb->r2_pos = v_scalar_mul(rb->r2_vel, dt);
+    }
+}
